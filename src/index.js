e7789ef33ffc1c37df699fc6f46b0fbd2cd26fa1
Andrew Sack <sacko87@gmail.com>
2019-02-23 08:26:03 -0500

    updates to work with n ew node_web_server

--- src/index.js
+++ src/index.js
@@ -1,24 +1,5 @@
-const createServer = require('node_web_server');
-const path = require('path');
-const fs = require('fs');
+const {app, server} = require('node_web_server');
 const router = require('./web/router.js');
 
-const rootDir = path.resolve(__dirname, '..')
-
-//load process.env variables from .env file
-require('dotenv').config();
-
-const port = process.env.PORT;
-let sslKeys = undefined;
-
-if(process.env.HTTPS === 'true') {
-   sslKeys = {
-    cert: fs.readFileSync(path.join(rootDir, process.env.CERT)),
-    key: fs.readFileSync(path.join(rootDir, process.env.KEY))
-  }
-}
-
-//create server.
-const {app, server} = createServer(port, sslKeys);
 //add routes
 router(app);
